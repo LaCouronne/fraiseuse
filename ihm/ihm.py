92b937bc941b9576c58a360c51ce7d84c858aa47
dodo <edwan@orange.fr>
2020-10-31 16:22:25 +0100

    nice ihm

--- ihm/ihm.py
+++ ihm/ihm.py
@@ -1,29 +1,18 @@
 import tkinter as tk
-from tkinter import ttk
 
 from objects.work import Work
 from objects.drill import Drill
 from objects.barrel import Barrel
 from objects.template import Template
 
-root = tk.Tk()
-root.title("First_Program")
-
-# Creating Menubar
-menubar = tk.Menu(root)
-
-# Adding Menu
-menubar.add_command(label="New job", command=None)
-menubar.add_command(label="Load job", command=None)
-root.config(menu=menubar)
 
 def pop_up_validation():
-    fInfos = tk.Toplevel()  # Popup -> Toplevel()
-    fInfos.title('Infos')
-    tk.Button(fInfos, text='Quitter', command=fInfos.destroy).pack(padx=10, pady=10)
-    fInfos.transient(root)  # Réduction popup impossible
-    fInfos.grab_set()  # Interaction avec fenetre jeu impossible
-    root.wait_window(fInfos)
+    f_infos = tk.Toplevel()  # Popup -> Toplevel()
+    f_infos.title('Infos')
+    tk.Button(f_infos, text='Quitter', command=f_infos.destroy).pack(padx=10, pady=10)
+    f_infos.transient(root)  # Réduction popup impossible
+    f_infos.grab_set()  # Interaction avec fenetre jeu impossible
+    root.wait_window(f_infos)
 
 
 def get_work_from_parameters():
@@ -36,7 +25,6 @@ def get_work_from_parameters():
 
 def start_job():
     work = get_work_from_parameters()
-    command = popup
     pop_up_validation()
     validated = work.validate()
 
@@ -47,70 +35,97 @@ def save_job():
     print("Validated: " + str(validated))
 
 
+root = tk.Tk()
+root.title("First_Program")
+root.attributes('-fullscreen', 1)
+
+# Creating Menubar
+menubar = tk.Menu(root)
+
+# Adding Menu
+menubar.add_command(label="New job", command=None)
+menubar.add_command(label="Load job", command=None)
+root.config(menu=menubar)
+
+# Work config frame
+form_frame = tk.Frame(root)
+form_frame.place(x=30, y=30)
+
+form_grid_pady = 10
+
 # Barrel form
-barrel_label = tk.Label(root, text='Barrel', font=('calibre', 15, 'bold'))
+barrel_frame = tk.Frame(form_frame)
+barrel_frame.grid(row=0, column=0, pady=form_grid_pady)
+
+barrel_label = tk.Label(barrel_frame, text='Barrel', font=('calibre', 15, 'bold'))
 barrel_label.grid(row=0, column=0)
 
 barrel_diameter_var = tk.DoubleVar()
-barrel_diameter_label = tk.Label(root, text='Diameter', font=('calibre', 10, 'bold'))
-barrel_diameter_entry = tk.Entry(root, textvariable=barrel_diameter_var, font=('calibre', 10, 'normal'))
-barrel_diameter_units_label = tk.Label(root, text='mm', font=('calibre', 10))
+barrel_diameter_label = tk.Label(barrel_frame, text='Diameter', font=('calibre', 10, 'bold'))
+barrel_diameter_entry = tk.Entry(barrel_frame, textvariable=barrel_diameter_var, font=('calibre', 10, 'normal'))
+barrel_diameter_units_label = tk.Label(barrel_frame, text='mm', font=('calibre', 10))
 barrel_diameter_label.grid(row=1, column=0)
 barrel_diameter_entry.grid(row=1, column=1)
 barrel_diameter_units_label.grid(row=1, column=2)
 
 barrel_height_var = tk.DoubleVar()
-barrel_height_label = tk.Label(root, text='Height', font=('calibre', 10, 'bold'))
-barrel_height_entry = tk.Entry(root, textvariable=barrel_height_var, font=('calibre', 10, 'normal'))
-barrel_height_units_label = tk.Label(root, text='mm', font=('calibre', 10))
+barrel_height_label = tk.Label(barrel_frame, text='Height', font=('calibre', 10, 'bold'))
+barrel_height_entry = tk.Entry(barrel_frame, textvariable=barrel_height_var, font=('calibre', 10, 'normal'))
+barrel_height_units_label = tk.Label(barrel_frame, text='mm', font=('calibre', 10))
 barrel_height_label.grid(row=2, column=0)
 barrel_height_entry.grid(row=2, column=1)
 barrel_height_units_label.grid(row=2, column=2)
 
-# Motif form 
-motif_label = tk.Label(root, text='Motif', font=('calibre', 15, 'bold'))
-motif_label.grid(row=4, column=0)
+# Motif form
+motif_frame = tk.Frame(form_frame)
+motif_frame.grid(row=1, column=0, pady=form_grid_pady)
+
+motif_label = tk.Label(motif_frame, text='Motif', font=('calibre', 15, 'bold'))
+motif_label.grid(row=0, column=0)
 
 template_height_var = tk.DoubleVar()
-template_height_label = tk.Label(root, text='Height', font=('calibre', 10, 'bold'))
-template_height_entry = tk.Entry(root, textvariable=template_height_var, font=('calibre', 10, 'normal'))
-template_height_units_label = tk.Label(root, text='mm', font=('calibre', 10))
-template_height_label.grid(row=5, column=0)
-template_height_entry.grid(row=5, column=1)
-template_height_units_label.grid(row=5, column=2)
+template_height_label = tk.Label(motif_frame, text='Height', font=('calibre', 10, 'bold'))
+template_height_entry = tk.Entry(motif_frame, textvariable=template_height_var, font=('calibre', 10, 'normal'))
+template_height_units_label = tk.Label(motif_frame, text='mm', font=('calibre', 10))
+template_height_label.grid(row=1, column=0)
+template_height_entry.grid(row=1, column=1)
+template_height_units_label.grid(row=1, column=2)
 
 template_width_var = tk.DoubleVar()
-template_width_label = tk.Label(root, text='Width', font=('calibre', 10, 'bold'))
-template_width_entry = tk.Entry(root, textvariable=template_width_var, font=('calibre', 10, 'normal'))
-template_width_units_label = tk.Label(root, text='mm', font=('calibre', 10))
-template_width_label.grid(row=6, column=0)
-template_width_entry.grid(row=6, column=1)
-template_width_units_label.grid(row=6, column=2)
+template_width_label = tk.Label(motif_frame, text='Width', font=('calibre', 10, 'bold'))
+template_width_entry = tk.Entry(motif_frame, textvariable=template_width_var, font=('calibre', 10, 'normal'))
+template_width_units_label = tk.Label(motif_frame, text='mm', font=('calibre', 10))
+template_width_label.grid(row=2, column=0)
+template_width_entry.grid(row=2, column=1)
+template_width_units_label.grid(row=2, column=2)
 
 nb_copy_var = tk.IntVar()
-nb_copy_label = tk.Label(root, text='Iterations', font=('calibre', 10, 'bold'))
-nb_copy_entry = tk.Entry(root, textvariable=nb_copy_var, font=('calibre', 10, 'normal'))
-nb_copy_label.grid(row=7, column=0)
-nb_copy_entry.grid(row=7, column=1)
+nb_copy_label = tk.Label(motif_frame, text='Iterations', font=('calibre', 10, 'bold'))
+nb_copy_entry = tk.Entry(motif_frame, textvariable=nb_copy_var, font=('calibre', 10, 'normal'))
+nb_copy_label.grid(row=3, column=0)
+nb_copy_entry.grid(row=3, column=1)
 
 # Drill form
-drill_label = tk.Label(root, text='Drill', font=('calibre', 15, 'bold'))
-drill_label.grid(row=8, column=0)
+drill_frame = tk.Frame(form_frame)
+drill_frame.grid(row=2, column=0, pady=form_grid_pady)
+
+drill_label = tk.Label(drill_frame, text='Drill', font=('calibre', 15, 'bold'))
+drill_label.grid(row=0, column=0)
 
 drill_diameter_var = tk.DoubleVar()
-drill_diameter_label = tk.Label(root, text='Diameter', font=('calibre', 10, 'bold'))
-drill_diameter_entry = tk.Entry(root, textvariable=drill_diameter_var, font=('calibre', 10, 'normal'))
-drill_diameter_units_label = tk.Label(root, text='mm', font=('calibre', 10))
-drill_diameter_label.grid(row=9, column=0)
-drill_diameter_entry.grid(row=9, column=1)
-drill_diameter_units_label.grid(row=9, column=2)
+drill_diameter_label = tk.Label(drill_frame, text='Diameter', font=('calibre', 10, 'bold'))
+drill_diameter_entry = tk.Entry(drill_frame, textvariable=drill_diameter_var, font=('calibre', 10, 'normal'))
+drill_diameter_units_label = tk.Label(drill_frame, text='mm', font=('calibre', 10))
+drill_diameter_label.grid(row=1, column=0)
+drill_diameter_entry.grid(row=1, column=1)
+drill_diameter_units_label.grid(row=1, column=2)
 
 
 # Options
-sub_btn = tk.Button(root, text='Start job', command=start_job)
-save_btn = tk.Button(root, text='Save job', command=save_job)
+sub_btn = tk.Button(form_frame, text='Start job', command=start_job)
+save_btn = tk.Button(form_frame, text='Save job', command=save_job)
 
-sub_btn.grid(row=10, column=0)
-save_btn.grid(row=10, column=1)
+sub_btn.grid(row=3, column=0, pady=form_grid_pady)
+save_btn.grid(row=3, column=1, pady=form_grid_pady)
 
 root.mainloop()
